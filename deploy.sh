3ea83b3f43c6b1f841d6887d5e647a2207bacf35
solgalain94 <solgalain@gmail.com>
2021-08-26 15:30:04 -0300

    index

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add .
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:solgalain94/articulos.git main:gh-pages
+
+
+cd -
